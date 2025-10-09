0414517daae9fe0290bcf4e8efdda6a1bd0128a4
Moises Santos <moisesbarbosa23@gmail.com>
2025-10-08 21:27:23 -0300

    chore: setup do projeto, reset de CSS e inserindo variáveis

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
