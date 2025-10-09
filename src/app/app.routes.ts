c664e9a769ee21e67f61ae92a1457d837513a3d7
Moises Santos <moisesbarbosa23@gmail.com>
2025-10-08 22:22:31 -0300

    feat: scaffold inicial dos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './features/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+  },
+  {
+    path: 'favoritos',
+    loadComponent: () => import('./features/favoritos/favoritos.component').then((c) => c.FavoritosComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
