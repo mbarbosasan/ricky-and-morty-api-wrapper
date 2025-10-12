b0963c79f75409975f55b17471429d008445c50b
Moises Santos <moisesbarbosa23@gmail.com>
2025-10-12 01:14:43 -0300

    chore: housekeeping

--- src/app/features/inicio/search-result/search-result.component.ts
+++ src/app/features/inicio/search-result/search-result.component.ts
@@ -2,13 +2,12 @@ import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core
 import { CharactersListComponent } from 'src/app/shared/ui/characters-list/characters-list.component';
 import { EmptyStateComponent } from 'src/app/shared/ui/empty-state/empty-state.component';
 import { FavoriteCharactersService } from '../../favoritos/services/favorite-characters.service';
-import { SearchEmptyStateComponent } from '../search-empty-state/search-empty-state.component';
 import { CharacterSearchResultWithFavorites, CharacterWithFavorite } from '../services/model/character.model';
 
 @Component({
   selector: 'app-search-result',
   standalone: true,
-  imports: [SearchEmptyStateComponent, CharactersListComponent, EmptyStateComponent],
+  imports: [CharactersListComponent, EmptyStateComponent],
   templateUrl: './search-result.component.html',
   styleUrl: './search-result.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
